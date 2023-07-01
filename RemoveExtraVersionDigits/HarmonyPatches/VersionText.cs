c3bba2d2a03d11dc1728018b14648ff4bc609ced
Saeraphinx <saeraphinx@gmail.com>
2023-07-01 00:39:15 -0500

    Add project files.

---
+++ RemoveExtraVersionDigits/HarmonyPatches/VersionText.cs
@@ -0,0 +1,42 @@
+﻿using HarmonyLib;
+using IPA.Loader;
+using System.Reflection;
+using System;
+using UnityEngine;
+using IPA.Utilities;
+using TMPro;
+
+namespace RemoveExtraVersionDigits.Patches
+{
+    internal class VersionText
+    {
+
+        [HarmonyPatch(typeof(SetApplicationVersionText))]
+        [HarmonyPatch("Start")]
+        internal class VersionTextPatch
+        {
+            //static bool Prepare() => Application.version.Contains("_");
+
+            // Prefix: Before the regular method runs - bool disables method.
+            private static bool Prefix(SetApplicationVersionText __instance) {
+                Plugin.Log.Info($"Game version {Application.version}");
+                TextMeshPro versionText = __instance.GetField<TextMeshPro, SetApplicationVersionText>("_versionText");
+                string currVersion = Application.version.Split('_')[0];
+                versionText.gameObject.transform.transform.position = new Vector3(0.0969f, 0.005f, -0.106f);
+                if (DateTime.Now.Day == 1 && DateTime.Now.Month == 4)
+                {
+                    versionText.text = "7.2.7";
+                } else if (DateTime.Now.Day == 27 && DateTime.Now.Month == 7)
+                {
+                    versionText.text = "7.2.7";
+                } else
+                {
+                    versionText.text = Application.version.Split('_')[0];
+                }
+                Plugin.Log.Info($"Moved foot text and set to {versionText.text}");
+
+                return false;
+            }
+        }
+    }
+}
---
+++ RemoveExtraVersionDigits/Plugin.cs
@@ -0,0 +1,73 @@
+﻿using HarmonyLib;
+using IPA;
+using IPA.Config;
+using IPA.Config.Stores;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+using UnityEngine;
+using IPALogger = IPA.Logging.Logger;
+
+namespace RemoveExtraVersionDigits
+{
+    [Plugin(RuntimeOptions.DynamicInit)]
+    public class Plugin
+    {
+        internal static Harmony HarmonyInstance { get; private set; }
+        public readonly string HarmonyID = "Saeraphinx.RemoveExtraVersionDigits";
+        internal static Plugin Instance { get; private set; }
+        internal static IPALogger Log { get; private set; }
+        internal static Assembly Assembly { get; } = Assembly.GetExecutingAssembly();
+
+        [Init]
+        public Plugin(IPALogger logger)
+        {
+            Instance = this;
+            Plugin.Log = logger;
+            Plugin.Log?.Debug("Logger initialized.");
+        }
+
+        [OnEnable]
+        public void OnEnable()
+        {
+            HarmonyInstance = new Harmony(HarmonyID);
+            ApplyHarmonyPatches();
+        }
+
+        [OnDisable]
+        public void OnDisable()
+        {
+            RemoveHarmonyPatches();
+        }
+
+        internal static void ApplyHarmonyPatches()
+        {
+            try
+            {
+                Plugin.Log?.Debug("Applying Harmony patches.");
+                HarmonyInstance.PatchAll(Assembly);
+            }
+            catch (Exception ex)
+            {
+                Plugin.Log?.Error("Error applying Harmony patches: " + ex.Message);
+                Plugin.Log?.Debug(ex);
+            }
+        }
+
+        internal static void RemoveHarmonyPatches()
+        {
+            try
+            {
+                HarmonyInstance.UnpatchSelf();
+            }
+            catch (Exception ex)
+            {
+                Plugin.Log?.Error("Error removing Harmony patches: " + ex.Message);
+                Plugin.Log?.Debug(ex);
+            }
+        }
+    }
+}
